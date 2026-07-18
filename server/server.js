71219f38bd29d2bc45dc152722573ae4dd667844
Mmokut Umoh <105234838+Yukin3@users.noreply.github.com>
2026-07-18 12:14:21 -0500

    Set up backand and API docs

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+
+
+const app = express();
+const PORT = 5000;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+
+//routes
+
+
+app.listen(PORT, () => {
+  console.log(`QueueSmart backend is running at http://localhost:${PORT}`);
+});
