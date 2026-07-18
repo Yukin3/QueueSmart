d0cd11d0baca7cf7c0459e3ceeb823be60587942
Mmokut Umoh <105234838+Yukin3@users.noreply.github.com>
2026-07-18 13:35:17 -0500

    Comment on usage

--- server/server.js
+++ server/server.js
@@ -9,67 +9,79 @@ const users = require("./data/users");
 
 
 const app = express();
-const PORT = 5000;
+const PORT = 5000; //port server runs on
 
 
-
-app.use(cors());
-app.use(express.json());
-
-
-
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-
-
-
-app.get("/api/health", (req, res) => {
-  res.json({ status: "ok" });
-});
+//middleware
+app.use(cors()); //allows front end/other origin requests
+app.use(express.json()); //convert json responses to javascript
 
 
 //routes
-app.get("/api/services", (req, res) => {
-  res.json(services);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html")); //root, this page has the API docs
 });
 
 
+app.get("/api/health", (req, res) => {
+  res.json({ status: "ok" }); //shows status of backend server
+});
 
 
-app.post("/api/auth/login", (req, res) => {
-  const { email, password } = req.body;
+//Routes
 
-  if (!email || !password) {
+
+
+    //Service
+app.get("/api/services", (req, res) => {
+  res.json(services); //returns list of mock services 
+});  //TODO: return services for specific admin/org, add  filtering methods
+ 
+
+
+    //Auth
+app.post("/api/auth/login", (req, res) => { //login handler
+  const { email, password } = req.body; //get email/pw from request
+
+  if (!email || !password) { //require both fields
     return res.status(400).json({
       error: "Email and password are required!",
     });
-  }
+  } 
 
-  const user = users.find(
+  
+  const user = users.find( //search for matching credentials in users
     (item) =>
-      item.email.toLowerCase() === email.toLowerCase() &&
+      item.email.toLowerCase() === email.toLowerCase() && //email isn't case sensitive
       item.password === password
   );
 
-  if (!user) {
+
+
+  if (!user) { //flag nonexistent user
     return res.status(401).json({
       error: "Invalid email or password!",
     });
   }
 
-  const { password: _password, ...safeUser } = user;
 
-  res.json({
+  const { password: _password, ...safeUser } = user;  //copy user without pw
+
+
+
+  res.json({ //return login success
     message: "Login success!",
     user: safeUser,
   });
-});
+
+
+});  //TODO: return services for specific admin/org, add  filtering methods, save user info to session(?)
 
 
 
+
+//start backend server
 app.listen(PORT, () => {
   console.log(`QueueSmart backend is running at http://localhost:${PORT}`);
 });
