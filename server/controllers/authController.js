26ec44d2f64fbb75d8093402c78b7c6e78df582e
Mmokut Umoh <105234838+Yukin3@users.noreply.github.com>
2026-07-24 23:19:55 -0500

    Add registration endpoint + validation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto");
+const { validateRegisterInput } = require("../utils/validation");
 const users = require("../data/users"); //import mock data, //TODO: replace w/ real data later
 
 function loginUser(req, res) {
@@ -40,7 +42,58 @@ function loginUser(req, res) {
 }
 
 
+function registerUser(req, res) {
+  const errors = validateRegisterInput(req.body); //validate registration input
+
+  //handle invalid registration
+  if (errors.length > 0) {
+    return res.status(400).json({
+      error: "Invalid registration input.",
+      details: errors,
+    });
+  }
+
+  const { name, email, password, role } = req.body;
+
+  //check if user alr exists
+  const existingUser = users.find(
+    (user) => user.email.toLowerCase() === email.toLowerCase()
+  );
+
+
+
+  if (existingUser) {
+    return res.status(409).json({error: "A user with this email already exists."}); //handle existing user
+  }
+
+
+  //create new user object
+  const newUser = {
+    id: `${role}-${crypto.randomUUID()}`,
+    name: name.trim(),
+    email: email.trim().toLowerCase(),
+    password,
+    role,
+    organizationId: "org-uh",
+  };
+
+
+  //add newUser to users
+  users.push(newUser);
+
+
+
+  const { password: _password, ...safeUser } = newUser;  //hide password before retuning user object
+
+
+  //return new user object
+  return res.status(201).json({
+    message: "Registration successful!",
+    user: safeUser,
+  });
+  
+}
+
+
 //export controller function
-module.exports = {
-  loginUser,
-};
+module.exports = {loginUser, registerUser};
