1358faedc76457cca19acd7f81ddb1553e7be27f
Mmokut Umoh <105234838+Yukin3@users.noreply.github.com>
2026-07-24 23:34:41 -0500

    Add registration endpoint + validation

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 
-const { loginUser } = require("../controllers/authController"); //import auth controller
+const { loginUser, registerUser } = require("../controllers/authController"); //import auth controller
 
 const router = express.Router();
 
 router.post("/login", loginUser); //handle post req
-router.post("/register", register);
+router.post("/register", registerUser);
 
 module.exports = router;
