7cbcbe6c9b92511da3562bb0aede9458e7867574
Mmokut Umoh <105234838+Yukin3@users.noreply.github.com>
2026-08-02 14:20:06 -0500

    Create empty User  model

--- server/models/UserCreds.js
+++ server/models/UserCreds.js
@@ -1,1 +1,19 @@
-//TODO: replace w mongo model
+const mongoose = require("mongoose")
+
+
+
+//TODO: fill in model schema
+const userCredModel = new mongoose.Schema(
+    {
+        email: {},
+        passwordHash: {},
+        role: {},
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+
+module.exports = mongoose.model("UserCreds", userCredModel)
--- server/models/UserProfile.js
+++ server/models/UserProfile.js
@@ -1,1 +1,20 @@
-//TODO: replace w mongo model
+const mongoose = require("mongoose")
+
+
+
+//TODO: fill in model schema
+const userProfileModel = new mongoose.Schema(
+    {
+        credsID: {},
+        fullName: {},
+        email: {},
+        phone: {},
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+
+module.exports = mongoose.model("UserProfile", userProfileModel)
