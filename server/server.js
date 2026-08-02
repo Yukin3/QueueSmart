6f2c2deec63dc33aa2b0239f6d70efcd8f6498d8
Mmokut Umoh <105234838+Yukin3@users.noreply.github.com>
2026-08-02 14:19:06 -0500

    Update server/db connection

--- server/server.js
+++ server/server.js
@@ -2,15 +2,24 @@ require("dotenv").config();
 
 const { connect } = require("mongoose");
 const app = require("./app");
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
 
 
 const PORT = process.env.PORT || 5000; //port server runs on
-connectDB(); //connect to mongoDB
 
 
 
-//start backend server
-app.listen(PORT, () => {
-  console.log(`QueueSmart backend is running at http://localhost:${PORT}`);
-});
+
+//start backend server after db connection
+async function startServer(){
+  await connectDB(); //connect to mongoDB
+
+  //start server
+  app.listen(PORT, () => {
+    console.log(`QueueSmart backend is running at http://localhost:${PORT}`);
+  });
+
+}
+
+
+startServer();
